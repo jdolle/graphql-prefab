4c7767b6a75a8dba4f7faebc78617dcd39f83429
Jeff Dolle <jdolle@gmail.com>
2018-03-25 21:58:51 -0700

    fix: velocity variables (#1)
    • Correctly return velocity compiled options
    • Adjust Query example to use an environment variable
    • Add environment variable to README test usage

--- src/lib/compileOptions.ts
+++ src/lib/compileOptions.ts
@@ -4,7 +4,7 @@ import { mapObjIndexed } from 'ramda'
 const compileOptions = (options?: ResolverConfig['options'], context?: {}) =>
   mapObjIndexed((value: OptionValue) => {
     if (Array.isArray(value)) {
-      (new Velocity.Compile(value)).render(context)
+      return (new Velocity.Compile(value)).render(context)
     }
     return value
   }, options)
