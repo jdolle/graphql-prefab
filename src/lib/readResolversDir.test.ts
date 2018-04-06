db35c323b693df43cffd7c09d40b2c9171b29050
Jeff Dolle <jdolle@gmail.com>
2018-04-06 01:35:30 -0700

    tests: add tests for parseConfig and readResolversDir (#15)
    • Add tests for parseConfig and readResolversDir

---
+++ src/lib/readResolversDir.test.ts
@@ -0,0 +1,14 @@
+import path from 'path'
+import readResolversDir from './readResolversDir'
+
+it('#readResolversDir gets all json files in the directory', () => {
+  const dirPath = path.resolve(__dirname, '../../examples/demo-server/resolvers')
+  const resolverPaths = readResolversDir(dirPath)
+  const relativePathArray = resolverPaths.map(p => path.relative(dirPath, p))
+
+  expect(relativePathArray).toMatchSnapshot()
+})
+
+it('#readResolversDir throws on an invalid path', () => {
+  expect(() => readResolversDir('./does_not_exist')).toThrow()
+})
