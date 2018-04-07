e16bc330f6a4eb50b24c4125a4bc84e08cc056d0
Jeff Dolle <jdolle@gmail.com>
2018-04-07 13:55:43 -0700

    misc: upgrade TS and test with moxios (#16)
    • Upgrade TS
    • Optimize TS build with importHelpers
    • Swap manual http axios mock with moxios
    • fix graphql-resolvers types

--- src/resolvers/http.test.ts
+++ src/resolvers/http.test.ts
@@ -1,15 +1,21 @@
-import axios from 'axios'
+import moxios from 'moxios'
 import httpResolver from './http'
 
-jest.mock('axios', () => jest.fn().mockResolvedValue({ data: {}}))
-
 it('#httpResolver throws if url is undefined', () => {
-  expect(() => httpResolver()()).toThrow()
-  expect(() => httpResolver({})()).toThrow()
+  expect(httpResolver()()).rejects.toMatchSnapshot()
+  expect(httpResolver({})()).rejects.toMatchSnapshot()
 })
 
-it('#httpResolver calls axios', () => {
-  httpResolver({ url: '/example' })()
+it('#httpResolver calls axios', (done) => {
+  moxios.withMock(() => {
+    const result = httpResolver({ url: '/example' })()
 
-  expect(axios).toHaveBeenCalledWith({ url: '/example' })
+    moxios.wait(async () => {
+      const request = moxios.requests.mostRecent()
+      await request.respondWith({ response: ['foo'] })
+      expect(request.url).toBe('/example')
+      expect(result).resolves.toEqual(['foo'])
+      done()
+    })
+  })
 })
