0e2d8d7a3934b4021657c97d7a5fdf70d433e6eb
Jeff Dolle <jdolle@gmail.com>
2018-04-13 00:35:27 -0700

    Fix strict null check errors; Allow  and  imports in jest

--- packages/graphql-prefab-server/src/index.ts
+++ packages/graphql-prefab-server/src/index.ts
@@ -3,8 +3,11 @@ import { importSchema } from 'graphql-import'
 import { compile } from 'graphql-prefab-resolvers'
 import path from 'path'
 
-const PREFAB_SCHEMA_PATH = process.env.PREFAB_SCHEMA_PATH || './types/Query.graphql'
-const PREFAB_RESOLVERS_PATH = process.env.PREFAB_RESOLVERS_PATH || './resolvers'
+const PREFAB_SCHEMA_PATH = process.env.PREFAB_SCHEMA_PATH === undefined ?
+  './types/Query.graphql' : process.env.PREFAB_SCHEMA_PATH
+
+const PREFAB_RESOLVERS_PATH = process.env.PREFAB_RESOLVERS_PATH === undefined ?
+  './resolvers' : process.env.PREFAB_RESOLVERS_PATH
 
 const start = async () => {
   const typeDefs = importSchema(path.resolve(PREFAB_SCHEMA_PATH))
@@ -20,7 +23,7 @@ const start = async () => {
 }
 
 process.on('uncaughtException', (err) => {
-  console.error((err && err.stack) ? err.stack : err)
+  console.error((err !== undefined && err.stack !== undefined) ? err.stack : err)
 })
 
 export default start
