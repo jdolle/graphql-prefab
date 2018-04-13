d5b7264599517a939c1a7e33b542e869b32409fc
Jeff Dolle <jdolle@gmail.com>
2018-04-12 18:02:32 -0700

    Allow configuring schema and resolvers paths, and pg via environment variables

--- packages/graphql-prefab-resolvers/src/resolvers/postgres.ts
+++ packages/graphql-prefab-resolvers/src/resolvers/postgres.ts
@@ -10,7 +10,13 @@ export interface PGConfig extends ResolverConfig {
 let _client: Pool | undefined
 const getClient = async () => {
   if (_client === undefined) {
-    _client = new Pool()
+    _client = new Pool({
+      user: process.env.PGUSER || process.env.DATABASE_USERNAME || 'admin',
+      host: process.env.PGHOST || process.env.DATABASE_HOST || 'localhost',
+      database: process.env.PGDATABASE || process.env.DATABASE_NAME || 'database',
+      password: process.env.PGPASSWORD || process.env.DATABASE_PASSWORD || 'password',
+      port: parseInt(process.env.PGPORT || process.env.DATABASE_PORT || '5432', 10),
+    })
     await _client.connect()
   }
 
--- packages/graphql-prefab-server/src/index.ts
+++ packages/graphql-prefab-server/src/index.ts
@@ -3,9 +3,12 @@ import { importSchema } from 'graphql-import'
 import { compile } from 'graphql-prefab-resolvers'
 import path from 'path'
 
+const PREFAB_SCHEMA_PATH = process.env.PREFAB_SCHEMA_PATH || './types/Query.graphql'
+const PREFAB_RESOLVERS_PATH = process.env.PREFAB_RESOLVERS_PATH || './resolvers'
+
 const start = async () => {
-  const typeDefs = importSchema(path.resolve(__dirname, 'types/Query.graphql'))
-  const resolvers = compile(path.resolve(__dirname, './resolvers'))
+  const typeDefs = importSchema(path.resolve(PREFAB_SCHEMA_PATH))
+  const resolvers = compile(path.resolve(PREFAB_RESOLVERS_PATH))
   const server = new GraphQLServer({
     typeDefs,
     resolvers,
@@ -15,5 +18,9 @@ const start = async () => {
     console.log('Server is running on localhost:4000')
   })
 }
+
+process.on('uncaughtException', (err) => {
+  console.error((err && err.stack) ? err.stack : err)
+})
 
 export default start
